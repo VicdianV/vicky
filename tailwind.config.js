ccfa8aaa36c37b4ae38c4b34e1550b2aef72d43e
fehrenbachmark <fehrenbachmark@gmail.com>
2023-08-05 09:52:05 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
